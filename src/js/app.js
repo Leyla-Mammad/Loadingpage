f9ea08269f2b6ec1fa3e7ae7b98b28db82b02e8f
Leyla-Mammad <leylamamedova19960@gmail.com>
2023-09-27 06:19:14 +0400

    adding js

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+    setTimeout(function() {
+      const loadingPage = document.querySelector('.loading-page');
+      loadingPage.style.display = 'none';
+
+     
+      const mainContent = document.getElementById('main-content');
+      mainContent.style.display = 'block';
+    }, 5000); 
